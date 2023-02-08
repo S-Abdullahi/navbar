5805ce9dd438374cf318231ed0fcb763c0f4e8f5
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-08 11:51:59 +0100

    menu dropdown

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,32 +1,40 @@
-import React from "react";
-import { FaBars, FaTwitter } from 'react-icons/fa';
-import {links, social} from './Data'
-import logo from './logo.svg'
+import React, {useState} from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { FaBehance, FaFacebook, FaLinkedin } from "react-icons/fa";
+import { links, social } from "./Data";
+import logo from "./logo.svg";
 
+export default function Navbar() {
+    const [show, setShow] = useState(false)
+  return (
+    <nav>
+      <div className="logo-con">
+        <img src={logo} alt="logo" className="logo" />
+        <FaBars className="menu-bar" onClick={()=>setShow(!show)}/>
+      </div>
+      <div className={`menu-con ${show && 'show'}`}>
+        <ul className="menu-unordered">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li>
+                <a href="#">{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-export default function Navbar(){
-    return (
-        <nav>
-            <div className="logo-con">
-                <img src={logo} alt='logo' className="logo"/>
-                <FaBars className="menu-bar"/>
-            </div>
-            <div className="menu-con">
-                <ul className="menu-unordered">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Home</a></li>
-                </ul>
-            </div>
-
-            <div className="icon-con">
-                <FaTwitter className="icon"/>
-                <FaTwitter className="icon"/>
-                <FaTwitter className="icon"/>
-                <FaTwitter className="icon"/>
-            </div>
-        </nav>
-    )
+      <div className="icon-con">
+        {social.map((socialIcon) => {
+          const { id, url, icon } = socialIcon;
+          return (
+            <a href={url} className="icon">
+              {icon}
+            </a>
+          );
+        })}
+      </div>
+    </nav>
+  );
 }
